0be003801fd845e54340ae6091fbfb996a7f6d4a
Jeferson Oliveira <adv.jefersonoliveira@gmail.com>
2024-01-15 16:40:42 -0300

    Update(aula): Aula 90 (exercício)

---
+++ section_03_logica_de_programacao/lesson90.py
@@ -0,0 +1,45 @@
+# 90. Exercício - crie uma lista de compras com listas
+
+'''
+Faça uma lista de compras com listas
+
+O usuário deve ter a possibilidade de inserir, apagar e listar valores de sua lista
+
+Não permita que o programa quebre com erros de índices inexistentes na lista.
+'''
+import os
+
+lista_de_compras = []
+
+while True:
+
+    menu = input(f'Escola uma opção: \n[1] Inserir item \n[2] Listar item \n[3] apagar item \n[4] Sair \n:')
+
+    if menu == '1':
+        os.system('cls')
+        add_item = input('Insira o item que deseja comprar: ')
+
+        if len(add_item) == 0:
+            print('Digite corretamente o item que deseja adicionar!')
+        
+        lista_de_compras.append(add_item)
+    
+    elif menu == '2':
+        os.system('cls')
+        for i in enumerate(lista_de_compras):
+            print(i)
+    
+    elif menu == '3':
+        os.system('cls')
+        excluir_item = input('Qual Produto deseja excluir?: ')
+
+        for i, j in enumerate(lista_de_compras):
+            if excluir_item == j:
+                lista_de_compras.remove(j)
+    
+    elif menu == '4':
+        print('Você saiu do programa! Até Logo!!!')
+        break
+    
+    else:
+        print('Opção inválida!')
