1fa79863ced153512d2a4958e7cd16d6e9791dd3
Jeferson Oliveira <adv.jefersonoliveira@gmail.com>
2024-01-17 21:55:13 -0300

    Update(aula): Aula 98-99 (Exercício) - Gerar o primeiro dígito  do CPF  após o traço

---
+++ section_03_logica_de_programacao/lesson98-99.py
@@ -0,0 +1,73 @@
+# 98-99. Exercício - Gerar o primeiro dígito de um CPF com Python
+"""
+Calculo do primeiro dígito do CPF: 746.824.890-70
+
+Colete a soma dos 9 primeiros dígitos do CPF multiplicando cada um dos valores por uma contagem regressiva começando de 10
+
+Ex.:  746.824.890-70 (746824890)
+    10  9  8  7  6  5  4  3  2
+*   7   4  6  8  2  4  8  9  0
+    70  36 48 56 12 20 32 27 0
+
+Somar todos os resultados: 70+36+48+56+12+20+32+27+0 = 301
+Multiplicar o resultado anterior por 10
+301 * 10 = 3010
+Obter o resto da divisão da conta anterior por 11
+3010 % 11 = 7
+Se o resultado anterior for maior que 9:
+    resultado é 0
+contrário disso:
+    resultado é o valor da conta
+
+O primeiro dígito do CPF é 7
+"""
+
+# Minha resposta:
+
+cpf_text = '746.824.890'
+cpf_limpo = cpf_text.replace('.','')
+
+acumulador = 0
+contador = 10
+
+for i, digito in enumerate(cpf_limpo):
+    acumulador += contador * int(digito)
+    contador -= 1
+
+multiplicador = acumulador * 10
+
+resto_divisão = multiplicador % 11
+
+if resto_divisão > 9:
+    
+    resto_divisão = 0
+    print(f'Primeiro dígito do cpf:{resto_divisão}')
+    cpf_text += f'-{str(resto_divisão)}'
+else:
+    resto_divisão
+    print(f'Primeiro dígito do cpf:{resto_divisão}')
+    cpf_text += f'-{str(resto_divisão)}'
+
+print(cpf_text)
+
+
+'''
+Sugestão de resposta:
+
+cpf = '746824890'
+
+nove_digitos = cpf[:9]
+
+contador_regresivo = 10
+
+for digito in nove_digitos:
+    resultado += int(digito) * contador_regresivo
+    contador_regresivo -= 1
+
+digito = (resultado * 10) % 11
+
+digito = digito if digito <= 9 else 0
+
+print(digito)
+
+'''
