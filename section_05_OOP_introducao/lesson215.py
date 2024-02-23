1313ab19dc2cb216169ab34108158ab5dc6bbc11
Jeferson Oliveira <adv.jefersonoliveira@gmail.com>
2024-02-23 13:55:26 -0300

    Upd(Aula): Complementação de infos.

--- section_05_OOP_introducao/lesson215.py
+++ section_05_OOP_introducao/lesson215.py
@@ -1,13 +1,23 @@
 # 215. Relações entre classes: associação, agregação e composição
-'''
+"""
+As relações entre classes em programação orientada a objetos (OOP) referem-se à 
+maneira como os objetos se relacionam entre si.
+
+É uma relação genérica entre objetos.
+
+Os objetos são independentes entre si, mas podem estar relacionados.
+
 Associação é um tipo de relação onde os objetos estão ligados dentro do sistema.
-Essa é a relação mais comum entre objetos e tem subconjuntos, 
-como agregação e composição.
+
+Essa é a relação mais comum entre objetos e tem subconjuntos, como agregação e composição.
 
 Geralmente, temos uma associação quando um objeto tem um atributo que referencia outro objeto.
 
 A associação não especifica como um objeto controla o ciclo de vida de outro objeto.
-'''
+
+Exemplo: A relação entre um professor e seus alunos. Um aluno pode ter vários 
+professores, e um professor pode ter vários alunos.
+"""
 
 class Escritor:
     def __init__(self, nome):
