6ed2c496fb35557363dcac46c9a049faf3f8c0f7
Jeferson Oliveira <adv.jefersonoliveira@gmail.com>
2024-02-26 16:15:18 -0300

    Upd(Aula): Inserido infos complementares

--- section_03_logica_de_programacao/lesson70-71.py
+++ section_03_logica_de_programacao/lesson70-71.py
@@ -1,5 +1,10 @@
 # 70-71. while - Qual letra apareceu mais vezes na frase? (Iterando strings com while)
-# tarefa - Qual a letra apareceu mais vezes na frase.
+"""tarefa - Qual a letra apareceu mais vezes na frase.
+
+Extra: var.count() -> O método count é utilizado para contar o número de ocorrências 
+de um determinado caractere ou substring em uma string.
+Ele retorna o número de vezes que o caractere ou substring aparece na string.
+"""
 
 phrase = 'O Python é uma linguagem de programação '\
     'multiparadigma. '\
@@ -22,8 +27,8 @@ while i < len(phrase):
     qtd_show_letter = phrase.count(actual_letter)
 
     if qtd_appears_more < qtd_show_letter:
-        qtd_appears_more =  qtd_show_letter
-        letter_appears_more  = actual_letter
+        qtd_appears_more = qtd_show_letter
+        letter_appears_more = actual_letter
     
     i += 1
 
